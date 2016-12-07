021fd495aa79d9b0fa824ad97ddd91f2b9a9e71d
ptitbob <github@shipstone.org>
2016-12-07 09:45:59 +0100

    Ajout correction 31

--- 31-jaxb/src/main/java/fr/apside/formation/jaxb/model/person/Address.java
+++ 31-jaxb/src/main/java/fr/apside/formation/jaxb/model/person/Address.java
@@ -3,6 +3,7 @@ package fr.apside.formation.jaxb.model.person;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Arrays;
 import java.util.List;
@@ -18,22 +19,25 @@ public class Address {
     /**
      * Identifiant
      */
-    @XmlAttribute
+    @XmlAttribute(name = "identifiant")
     public Long id;
 
     /**
      * Liste des ligne d'adresse (rue, étage, etc...)
      */
+    @XmlElement(name = "Ligne")
     public List<String> lineList;
 
     /**
      * Code postal
      */
+    @XmlElement(name = "code_postal")
     public String zipcode;
 
     /**
      * Ville
      */
+    @XmlElement(name = "ville")
     public String town;
 
     public Address() {
